1087eba3a7b35eccb31cddbca55aecdb21e09c71
GuillaumeLandwerlin58 <guillaume.landwerlin@gmail.com>
2023-09-20 16:22:45 +0200

    question 2.2

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,45 @@
 import Image from 'next/image'
+import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
+import {BreadCrumbs, Button, SectionContainer} from "tp-kit/components";
+import {ProductCardLayout, ProductGridLayout} from "tp-kit/components/products";
+import ProductFilter from "@/components/product-filter";
+const categories = PRODUCTS_CATEGORY_DATA;
 
 export default function Home() {
-  return (
-    <main>
+  console.log(categories)
+  return (<main>
+        <SectionContainer>
+          <BreadCrumbs
+              items={[
+                {
+                  label: 'Accueil',
+                  url: '#',
+                }
+              ]}
+          />
+            <ProductFilter categories={categories}/>
+          {categories.map((category) => (
+              <>
+                <h1 className="text-4xl font-bold">{category.name}({category.products.length})</h1>
+                <br></br>
+                <ProductGridLayout products={category.products}>
+                  {(product) => <ProductCardLayout product={product} button={<Button fullWidth variant="ghost">Click me</Button>}/>}
 
-    </main>
+                </ProductGridLayout>
+
+              </>
+          ))}
+
+
+          {/*(product) => <pre>{JSON.stringify(product,null,2)}</pre>*/}
+          {/*categories.map((product) => <ProductCardLayout product={product} />)*/}
+
+
+
+
+        </SectionContainer>
+
+
+      </main>
   )
 }
