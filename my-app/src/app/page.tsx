ba8e0f2c318047411893c2a170326759f369a812
GuillaumeLandwerlin58 <guillaume.landwerlin@gmail.com>
2023-09-22 11:55:37 +0200

    avancement

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -5,41 +5,12 @@ import {ProductCardLayout, ProductGridLayout} from "tp-kit/components/products";
 import ProductFilter from "@/components/product-filter";
 const categories = PRODUCTS_CATEGORY_DATA;
 
+import Productlist from "@/components/productlist";
+
 export default function Home() {
-  console.log(categories)
-  return (<main>
-        <SectionContainer>
-          <BreadCrumbs
-              items={[
-                {
-                  label: 'Accueil',
-                  url: '#',
-                }
-              ]}
-          />
-            <ProductFilter categories={categories}/>
-          {categories.map((category) => (
-              <>
-                <h1 className="text-4xl font-bold">{category.name}({category.products.length})</h1>
-                <br></br>
-                <ProductGridLayout products={category.products}>
-                  {(product) => <ProductCardLayout product={product} button={<Button fullWidth variant="ghost">Click me</Button>}/>}
-
-                </ProductGridLayout>
-
-              </>
-          ))}
-
-
-          {/*(product) => <pre>{JSON.stringify(product,null,2)}</pre>*/}
-          {/*categories.map((product) => <ProductCardLayout product={product} />)*/}
-
-
-
-
-        </SectionContainer>
-
-
-      </main>
+  return (
+    <div>
+      <Productlist categories={categories} />
+    </div>
   )
 }
---
+++ my-app/src/utils/filter-products.ts
@@ -0,0 +1,9 @@
+import { ProductsCategoryData } from "tp-kit/types";
+import { ProductFiltersResult } from "../types";
+
+export function filterProducts(
+    categories: ProductsCategoryData[],
+    filters?: ProductFiltersResult
+) : ProductsCategoryData[] {
+    // ...
+};
